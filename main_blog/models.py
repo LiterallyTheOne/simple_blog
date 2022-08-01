4dee4c29cf97a9fdfa7b0ae9dbb7f54633c97f3c
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2022-08-01 20:47:47 +0430

    add Tag and Post to models.py

--- main_blog/models.py
+++ main_blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    color = models.IntegerField()
+    name = models.CharField(max_length=20, unique=True)
+    img = models.ImageField(upload_to='tag_images', default=None)
+    url = models.URLField(default='', blank=True)
+    description = models.TextField(default='no description yet')
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_tags_order_by_more_posts():
+        return Tag.objects.all().annotate(num_posts=models.Count('post')).order_by('-num_posts')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='post_images', default=None)
+    description = models.TextField(default='no description yet')
+    publish_date = models.DateTimeField(auto_now_add=True)
+    edit_date = models.DateTimeField(auto_now=True)
+
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-publish_date"]
