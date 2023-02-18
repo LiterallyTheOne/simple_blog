9eefd72efcd6859924fe562457b6c207d9450800
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2023-02-18 10:22:57 +0330

    Add templates, js, css, manifest and logos

---
+++ main_blog/static/main_blog/r_nav_creator.js
@@ -0,0 +1,68 @@
+function create_li_for_nav(target_id, target_content, margins) {
+    li_1 = document.createElement('li');
+
+    var li_1_class_attr = 'nav-item';
+
+    for (var m1 in margins) {
+        li_1_class_attr += ` ${margins[m1]}`;
+    }
+
+    li_1.setAttribute('class', li_1_class_attr);
+
+    a_1 = document.createElement('a');
+    a_1.setAttribute('class', 'nav-link');
+    a_1.setAttribute('href', `#${target_id}`);
+    a_1.innerHTML = target_content;
+
+    li_1.appendChild(a_1);
+
+    return li_1;
+
+}
+
+function create_ul_for_nested_elements() {
+    ul_1 = document.createElement('ul');
+    ul_1.setAttribute('class', 'nav nav-pills list-group');
+    return ul_1;
+}
+
+var r1 = document.getElementById('r-nav-ul');
+
+var post_description = document.getElementById('post-description');
+
+
+var hs = post_description.querySelectorAll("h2, h3, h4, h5, h6");
+
+if (hs.length > 0){
+    var current = 0;
+    var current_h = hs[0].tagName.slice(1);
+
+    var add_to = [r1];
+
+    var perv_k1 = null;
+
+    for (var i = 0; i < hs.length; i++) {
+
+        if (current_h < hs[i].tagName.slice(1)) {
+            current++;
+            var target_ul = create_ul_for_nested_elements();
+            perv_k1.appendChild(target_ul);
+            add_to.push(target_ul);
+
+        } else if (current_h > hs[i].tagName.slice(1)) {
+            current -= parseInt(current_h) - parseInt(hs[i].tagName.slice(1));
+            for(var j=0; j < (parseInt(current_h) - parseInt(hs[i].tagName.slice(1))); j++){
+                add_to.pop();
+            }
+
+        }
+        current_h = hs[i].tagName.slice(1);
+
+        var k1 = create_li_for_nav(hs[i].getAttribute('id'), hs[i].innerHTML, ['my-1', `ms-${current+2}`]);
+
+        perv_k1 = k1;
+
+        add_to[current].appendChild(k1);
+    }
+}
+
---
+++ main_blog/static/main_blog/r_pagination.js
@@ -0,0 +1,125 @@
+
+function create_li_with_logo(page_value, inner_html, disabled, aria_label) {
+    var li_1 = document.createElement('li');
+
+    var li_1_class_attr = 'page-item';
+
+    if (disabled) {
+        li_1_class_attr += ' disabled';
+    }
+
+    li_1.setAttribute('class', li_1_class_attr);
+
+    var a_1 = document.createElement('a');
+    a_1.setAttribute('class', 'page-link')
+    a_1.setAttribute('href', `?page=${page_value}`);
+    a_1.setAttribute('aria-label', aria_label);
+
+    li_1.appendChild(a_1)
+
+
+    var span_1 = document.createElement('span');
+    span_1.setAttribute('aria-hidden', 'true');
+    span_1.innerHTML = inner_html;
+
+    a_1.appendChild(span_1);
+
+    return li_1;
+
+}
+
+
+function create_li_for_pagination(page_value, activate = false) {
+    var li_1 = document.createElement('li');
+    var li_1_class_attr = 'page-item';
+
+    if (activate) {
+        li_1_class_attr += ' active';
+    }
+    li_1.setAttribute('class', li_1_class_attr);
+
+    var a_1 = document.createElement('a');
+    a_1.setAttribute('class', 'page-link');
+    a_1.setAttribute('href', `?page=${page_value}`)
+    a_1.innerHTML = `${page_value}`;
+    li_1.appendChild(a_1);
+
+    return li_1;
+
+}
+
+function create_li_3_dots(){
+    var li_1 = document.createElement('li');
+    li_1.setAttribute('class', 'page-item disabled');
+
+    var a_1 = document.createElement('a');
+    a_1.setAttribute('class', 'page-link');
+    a_1.innerHTML = '...';
+
+    li_1.appendChild(a_1);
+    
+    return li_1;
+}
+
+if(number_of_pages > 0){
+
+    var r1 = document.getElementById('r_pagination');
+
+
+    li_previous = create_li_with_logo(current_page_start_from_1 - 1, "&laquo;", current_page_start_from_1 == 1, 'Previous');
+
+    r1.appendChild(li_previous);
+
+
+    li_1 = create_li_for_pagination(1, current_page_start_from_1 == 1);
+
+    r1.appendChild(li_1);
+
+
+    if (current_page_start_from_1 - 2 > 1) {
+        li_3_dots = create_li_3_dots();
+
+        r1.appendChild(li_3_dots);
+    }
+
+    if (current_page_start_from_1 - 1 > 1) {
+        li_before_current = create_li_for_pagination(current_page_start_from_1 - 1, false);
+
+        r1.appendChild(li_before_current);
+    }
+
+    if (current_page_start_from_1 != 1 && current_page_start_from_1 != number_of_pages) {
+        li_current = create_li_for_pagination(current_page_start_from_1, true);
+
+        r1.appendChild(li_current);
+    }
+
+    if (current_page_start_from_1 + 1 < number_of_pages) {
+        li_after_current = create_li_for_pagination(current_page_start_from_1 + 1, false);
+
+        r1.appendChild(li_after_current);
+    }
+
+    if (current_page_start_from_1 + 2 < number_of_pages) {
+        li_3_dots = create_li_3_dots();
+
+        r1.appendChild(li_3_dots);
+    }
+
+
+    if (number_of_pages > 1) {
+        li_last = create_li_for_pagination(number_of_pages, current_page_start_from_1 == number_of_pages);
+
+        r1.appendChild(li_last);
+    }
+
+
+    li_next = create_li_with_logo(current_page_start_from_1 + 1, "&raquo;", current_page_start_from_1 == number_of_pages, 'Next');
+
+    r1.appendChild(li_next);
+}
+
+
+
+
+
