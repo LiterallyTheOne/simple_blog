2255af5f5d0cbcefb82704bf6d35484ad6f5d601
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2023-02-20 17:05:08 +0330

    Reformat the result of docutils

--- r_utils.py
+++ r_utils.py
@@ -4,7 +4,12 @@ import re
 
 def rst_to_html(rst_text: str) -> str:
     result = docutils.core.publish_parts(source=rst_text, writer_name='html')['body']
+    # make headers smaller to have the main header in h1
     for i in range(5, 0, -1):
         result = result.replace(f'h{i}', f'h{i + 1}')
-    # result = re.sub(r'<h(\d)>([^<]*)', r'<h\1 id="\2">\2', result)
+    # change cite to code to have the style that I want
+    result = result.replace('<cite>', '<code class="rounded p-1">')
+    result = result.replace('</cite>', '</code>')
+    # change pre to
+    result = re.sub(r'<pre class="literal-block">((.|\s)*?)(?=<\/pre>)', r'<pre><code>\1</code></pre', result)
     return result
