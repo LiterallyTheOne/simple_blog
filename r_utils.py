a029590e32a42ebc85285a759b4ec9dadbfad2b8
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2023-02-26 17:29:26 +0330

    Fix the url for figures in rst

--- r_utils.py
+++ r_utils.py
@@ -3,7 +3,9 @@ import re
 
 
 def rst_to_html(rst_text: str) -> str:
-    result = docutils.core.publish_parts(source=rst_text, writer_name='html')['body']
+    # TODO I need to make it more general
+    result = docutils.core.publish_parts(
+        source=rst_text, writer_name='html')['body']
     # make headers smaller to have the main header in h1
     for i in range(5, 0, -1):
         result = result.replace(f'h{i}', f'h{i + 1}')
@@ -11,6 +13,12 @@ def rst_to_html(rst_text: str) -> str:
     result = result.replace('<cite>', '<code class="rounded p-1">')
     result = result.replace('</cite>', '</code>')
     # change pre to
-    result = re.sub(r'<pre class="literal-block">((.|\s)*?)(?=<\/pre>)', r'<pre><code>\1</code></pre>', result)
-    result = re.sub(r'<pre class="code literal-block">((.|\s)*?)(?=<\/pre>)', r'<pre><code>\1</code></pre>', result)
+    result = re.sub(r'<pre class="literal-block">((.|\s)*?)(?=<\/pre>)',
+                    r'<pre><code>\1</code></pre>', result)
+    result = re.sub(r'<pre class="code literal-block">((.|\s)*?)(?=<\/pre>)',
+                    r'<pre><code>\1</code></pre>', result)
+
+    # A navie solution to fix the path problems
+    result = result.replace('src="media', 'src="/media')
+
     return result
