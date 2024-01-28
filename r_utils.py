82e3d685f2c709279fed5e94adb00725688a4828
Ramin Zarebidoky <Ramin.zarebidoky@gmail.com>
2024-01-28 18:04:11 +0330

    Make the replacing `pre` with `pre` `code` more general

--- r_utils.py
+++ r_utils.py
@@ -14,9 +14,9 @@ def rst_to_html(rst_text: str) -> str:
     result = result.replace('<cite>', '<code class="rounded p-1">')
     result = result.replace('</cite>', '</code>')
     # change pre to
-    result = re.sub(r'<pre class="literal-block">((.|\s)*?)(?=<\/pre>)',
+    result = re.sub(r'<pre class="literal-block">((.|\s)*?)(?=</pre>)',
                     r'<pre><code>\1</code></pre>', result)
-    result = re.sub(r'<pre class="code literal-block">((.|\s)*?)(?=<\/pre>)',
+    result = re.sub(r'<pre class="code .* literal-block">((.|\s)*?)(?=</pre>)',
                     r'<pre><code>\1</code></pre>', result)
 
     # A naive solution to fix the path problems
